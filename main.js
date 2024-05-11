7419cef2a30a0d30dbd19783ec76ca16fdd6ef66
Gerardo Espínola <61892591+Gerardo-PY@users.noreply.github.com>
2024-05-10 20:29:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ document.getElementById("anio").innerHTML = "&copy " + anio + " | Radio Siete Vi
 const stream_list = [
     {
         //music : "https://s2.free-shoutcast.com/stream/18044/;"
-        music: "https://colombia-flags-sewing-ann.trycloudflare.com",
+        music: "https://teaching-philippines-somalia-everyday.trycloudflare.com",
         mountPoint: "/stream"
     }
 ];
