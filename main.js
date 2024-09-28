d770344af29c62a1a628cadbaa853e25da71d393
Gerardo Espínola <61892591+Gerardo-PY@users.noreply.github.com>
2024-09-27 20:33:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ document.getElementById("anio").innerHTML = "&copy " + anio + " | Radio Siete Vi
 const stream_list = [
     {
         //music : "https://s2.free-shoutcast.com/stream/18044/;"
-        music: "https://innovative-conservation-doom-thompson.trycloudflare.com",
+        music: "https://tiffany-fingers-turkey-garage.trycloudflare.com",
         mountPoint: "/stream"
     }
 ];
