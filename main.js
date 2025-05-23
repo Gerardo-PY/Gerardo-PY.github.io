ac8ce8c2ef3bf5436a7907791921577b498ab647
Gerardo Espínola <61892591+Gerardo-PY@users.noreply.github.com>
2025-05-23 20:21:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ document.getElementById("anio").innerHTML = "&copy " + anio + " | Radio Siete Vi
 const stream_list = [
     {
         //music : "https://s2.free-shoutcast.com/stream/18044/;"
-        music: "https://catalogs-vertical-harley-educational.trycloudflare.com",
+        music: "https://texture-presently-wednesday-posting.trycloudflare.com",
         mountPoint: "/stream"
     }
 ];
