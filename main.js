5d57c4f192b72155364cc99eb9c5069f57a5d74f
Gerardo-PY <gerardoespinola1997@gmail.com>
2026-08-09 14:56:21 -0300

    update stream url

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ document.getElementById("anio").innerHTML = "&copy " + anio + " | Radio Siete Vi
 const stream_list = [
     {
         //music : "https://s2.free-shoutcast.com/stream/18044/;"
-        music: "https://communications-physiology-discusses-opposite.trycloudflare.com",
+        music: "https://want-animation-older-consecutive.trycloudflare.com",
         mountPoint: "/stream",
         token: ""
     }
